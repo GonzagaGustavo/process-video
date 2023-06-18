4cacc9d2fd234deaa5a31537f777da57944808d2
Gustavo Gonzaga <Gustavogonzaga.gg243@gmail.com>
2023-06-17 22:40:42 -0300

    comprimindo video e tranformando para resoluções desejadas
    comprimindo video e tranformando para resoluções desejadas de acordo com a resolução do video.

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Projeto criado para aprender a tratar, comprimir e transformar em vários formatos e qualidades videos.
 
 Para depois implementar ao <a href="https://github.com/GonzagaGustavo/social-network">Social Network</a>
+51.703KB
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-import ffmpeg from "fluent-ffmpeg";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { path as ffmpegPath } from "@ffmpeg-installer/ffmpeg";
@@ -7,27 +6,99 @@ import { spawn } from "node:child_process";
 const file = path.dirname(fileURLToPath(import.meta.url)) + "/videos/video.mp4";
 const basename = "video";
 
-console.log(ffmpegPath);
-
-try {
+function videoTo144p() {
   const ffmpeg = spawn(ffmpegPath, [
     `-i`,
     `${file}`,
     "-vcodec",
-    "libx264",
+    "libx265",
+    "-preset",
+    "ultrafast",
+    "-c:a",
+    "copy",
     "-vf",
-    `scale=256:144`,
-    `${basename}-256x144.mp4`,
+    "scale=-2:144",
+    `${basename}-144p.mp4`,
   ]);
-
-  ffmpeg.stderr.on("data", (stream) => {
-    console.log(stream);
-  });
-
-  ffmpeg.stdout.on("data", (stream) => {
-    console.log(stream);
-  });
-} catch (err) {
-  console.error(err);
-  throw err;
 }
+function videoTo480p() {
+  const ffmpeg = spawn(ffmpegPath, [
+    `-i`,
+    `${file}`,
+    "-vcodec",
+    "libx265",
+    "-preset",
+    "ultrafast",
+    "-c:a",
+    "copy",
+    "-vf",
+    "scale=-2:480",
+    `${basename}-480p.mp4`,
+  ]);
+}
+function videoTo720p() {
+  const ffmpeg = spawn(ffmpegPath, [
+    `-i`,
+    `${file}`,
+    "-vcodec",
+    "libx265",
+    "-preset",
+    "ultrafast",
+    "-c:a",
+    "copy",
+    "-vf",
+    "scale=-2:720",
+    `${basename}-720p.mp4`,
+  ]);
+}
+function videoTo1080p() {
+  const ffmpeg = spawn(ffmpegPath, [
+    `-i`,
+    `${file}`,
+    "-vcodec",
+    "libx265",
+    "-preset",
+    "ultrafast",
+    "-c:a",
+    "copy",
+    "-vf",
+    "scale=-2:1080",
+    `${basename}-720p.mp4`,
+  ]);
+}
+
+function getResolution() {
+  return new Promise((resolve, reject) => {
+    const video = spawn(ffmpegPath, ["-i", `${file}`]);
+
+    video.stderr.on("data", (data) => {
+      const match = data.toString().match(/Stream.*Video:.*\s(\d+)x(\d+)/);
+
+      if (match && match[2]) resolve(match[2]);
+    });
+
+    video.stderr.on("error", (err) => reject(err));
+  });
+}
+
+async function processVideo() {
+  const resolution = await getResolution();
+
+  if (resolution >= 1080) {
+    videoTo1080p();
+    videoTo720p();
+    videoTo480p();
+    videoTo144p();
+  } else if (resolution < 1080 && resolution >= 720) {
+    videoTo720p();
+    videoTo480p();
+    videoTo144p();
+  } else if (resolution < 720 && resolution >= 480) {
+    videoTo480p();
+    videoTo144p();
+  } else if (resolution < 480 && resolution >= 144) {
+    videoTo144p();
+  }
+}
+
+processVideo();
